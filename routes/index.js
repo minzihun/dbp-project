7456744c310cd3feb1b9abfcd97828b5eae5d56b
조은학 <02069@naver.com>
2022-05-08 13:00:49 +0900

    feat 초기 이닛페이지

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 회원가입
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Signup' });
+});
+
+
+//로그인
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+
 module.exports = router;
