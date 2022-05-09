1da41021ae17441237db42233db59c12f42df335
mieun <45choi22@gmail.com>
2022-05-09 13:09:42 +0900

    employee change

--- routes/employee.js
+++ routes/employee.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const db_con = require('../db_con');
 
 // 직원- 프로젝트 관리
 router.get('/projectList', function(req, res, next) {
@@ -12,8 +11,5 @@ router.get('/createProject', function(req, res, next) {
     res.render('employee/createProject', { title: 'createProject' });
   });
 
-router.get('/my_page', (req,res)=>{
-
-})
 
 module.exports = router;
