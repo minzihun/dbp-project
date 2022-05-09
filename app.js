f93c7204b8981ec241625b08e49fb27972b54118
mieun <45choi22@gmail.com>
2022-05-09 10:42:28 +0900

    0509_10

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var employeeRouter = require('./routes/employee');
 var managerRouter = require('./routes/manager');
 
+dotenv.config();
+
 var app = express();
 
+
+app.set('views', './views')
+app.set('view engine','pug')
 
 app.use(logger('dev'));
 app.use(express.json());
