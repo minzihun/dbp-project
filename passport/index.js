9b1aecc868a272ec42c9efbc2b214e9c0459c22b
mieun <45choi22@gmail.com>
2022-05-12 15:47:25 +0900

    complete auth

--- passport/index.js
+++ passport/index.js
@@ -1,25 +1,25 @@
-const passport = require('passport');
-const local = require('./localStrategy');
-const Employee = require('../models/employee');
-const Manager = require('../models/manager');
+const passport = require("passport");
+const local = require("./localStrategy");
+const Employee = require("../models/employee");
+const Manager = require("../models/manager");
 
-module.exports = () =>{
-    passport.serializeUser((user, done)=>{
-        done(null, user.id)
-    });
-    passport.deserializeUser((id, done)=>{
-        Employee.findOne({where:{id}})
-            .then(async user => {
-                const admin = await Manager.findOne({where:{Employee_number:id}});
-                if(admin){
-                    user.isAdmin = true;
-                }else{
-                    user.isAdmin = false;
-                }
-                done(null, user)
-            })
-            .catch(err => done(err));
-    });
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    Employee.findOne({ where: { id } })
+      .then(async (user) => {
+        // const admin = await Manager.findOne({where:{Employee_number:id}});
+        // if(admin){
+        //     user.isAdmin = true;
+        // }else{
+        //     user.isAdmin = false;
+        // }
+        done(null, user);
+      })
+      .catch((err) => done(err));
+  });
 
-    local();
+  local();
 };
