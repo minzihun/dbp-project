6bc510285eff32b4e7664efde7effc46c4f6b5a6
mieun <45choi22@gmail.com>
2022-05-13 20:24:21 +0900

    fetch state 0513

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,29 @@
 var express = require("express");
 var router = express.Router();
-const Employee = require("../models/employee");
+const { Employee, Manager } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 const { getUser, getDeptId } = require("./user");
 
 var state;
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  console.log("======================================================");
-  // console.log(req.user);
+router.get("/", async function (req, res, next) {
   if (!req.user) {
     state = "beforeLogin";
   } else {
-    var { isAdmin } = req.user;
+    const isAdmin = await Manager.findOne({
+      where: { Employee_number: req.user.id },
+    });
     if (isAdmin) {
-      state = "management";
+      state = "manager";
     } else {
-      state = "developer";
+      state = "employee";
     }
   }
-  console.log(state);
   res.render("index", { title: "Express", state });
 });
 
-// // 회원가입
-// router.get('/signup', function(req, res, next) {
-//   res.render('signup', { title: 'Signup' });
-// });
-
-// router.post('/signup', (req, res)=>{
-
-// })
-
-// //로그인
-// router.get('/login', function(req, res, next) {
-//   res.render('login', { title: 'Login' });
-// });
-
 //마이페이지
 router.get("/mypage", isLoggedIn, function (req, res, next) {
-  console.log(state);
   const current_user = getUser(req.user);
   res.render("mypage", { title: "Mypage", state, current_user });
 });
@@ -54,16 +38,26 @@ router.get("/updateMyInfo", isLoggedIn, (req, res, next) => {
 router.post("/updateMyInfo", isLoggedIn, async (req, res, next) => {
   const { name, final_edu, skill, career, dept } = req.body;
   const Dept_id = getDeptId(dept);
-  console.log("변화전", req.user);
   try {
     const afterEmp = await Employee.update(
       { emp_name: name, emp_final_edu: final_edu, skill, career, Dept_id },
       { where: { id: req.user.id } }
     );
-    console.log("변화후", afterEmp);
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+//아이디 중복체크
+router.post("/checkId", async (req, res, next) => {
+  const id = req.body.rawID;
+  if (id) {
+    const exEmp = await Employee.findOne({ where: { emp_ID: id } });
+
+    if (exEmp) {
+      res.send("이미 있는 사용자입니다.");
+    }
   }
 });
 
@@ -72,12 +66,5 @@ const { isAdmin } = require("./middlewares");
 router.get("/isadmin", isLoggedIn, isAdmin, (req, res, next) => {
   res.send(req.user);
 });
-
-//데이터 확인용(지울 것)
-const { Dept } = require("../models");
-const { Emp_Proj } = require("../models");
-const { Manager } = require("../models");
-const { Project } = require("../models");
-const { Role } = require("../models");
 
 module.exports = router;
