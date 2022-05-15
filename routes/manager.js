f89ee4ad5975949fdca20f0f185ae66c4636c1c8
JihunMin <minzihun@gmail.com>
2022-05-16 08:24:56 +0900

    feat: manager_searchEmployee view

--- routes/manager.js
+++ routes/manager.js
@@ -3,8 +3,22 @@ var router = express.Router();
 
 
 // 경영진 - 직원검색
-router.get('/searchEmployee',function(req, res, next) {
-    res.render('manager/searchEmployee', { title: 'searchEmployee' });
+// 직원 id나 이름 받아오고 /searchEmplyee/:name(id나 이름) 페이지로 redirect 해준다.
+router.post('/searchEmployee',function(req, res, next) {
+  // res.render('manager/searchEmployee', { title: 'searchEmployee' });
+});
+
+// 경영진 - 직원 전체 목록, 검색된 직원 정보 + 프로젝트 내역
+router.get('/searchEmployee','/searchEmployee/:name',function(req, res, next) {
+    // res.render('manager/searchEmployee', { title: 'searchEmployee' });
+
+    var name = req.params.name;
+      // id나 직원이름으로 들어온 url이면 해당 직원정보(직원번호, 부서명, 이름, 최종학력, 기술, 경력)랑 프로젝트정보(프로젝트명, 역할, 예산, 발주처, 시작일, 종료일) 데이터 보내줌
+      if(name){
+
+      } else { // 초기화면은 전체 직원정보(직원번호, 부서명, 이름, 최종학력, 기술, 경력) 데이터 보내줌
+
+      }
   });
 
 // 경영진 - 프로젝트검색
