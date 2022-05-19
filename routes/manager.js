e04d92508166db098a198d8c112d32993296f63c
mieun <45choi22@gmail.com>
2022-05-20 00:17:11 +0900

    종혁님 작업사항

--- routes/manager.js
+++ routes/manager.js
@@ -12,17 +12,29 @@ router.get("/searchEmployee", function (req, res, next) {
   });
 });
 
-// // 경영진 - 직원검색
-// // 직원 id나 이름 받아오고 /searchEmplyee/:name(id나 이름) 페이지로 redirect 해준다.
-// router.post("/searchEmployee", function (req, res, next) {
-//   const { selected_search_key, search_key } = req.body;
-//   console.log(selected_search_key, search_key);
-//   const result = ///
-//     res.render("/employee", { result });
-//   // res.render('manager/searchEmployee', { title: 'searchEmployee' });
-// });
+// 경영진 - 직원검색
+// 직원 id나 이름 받아오고 /searchEmplyee/:name(id나 이름) 페이지로 redirect 해준다.
+router.get("/searchEmployee", async (req, res, next) => {
+  const { id, emp_name, Dept_id, emp_final_edu, skill, career } = req.body;
+  try {
+    const initemp = await Employee.findAll({
+      attributes: [
+        "id",
+        "emp_name",
+        "Dept_id",
+        "emp_final_edu",
+        "skill",
+        "career",
+      ],
+    });
+    res.render("manager/searchEmployee", { title: "searchEmployee", initemp });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
-// // 경영진 - 직원 전체 목록, 검색된 직원 정보 + 프로젝트 내역
+// 경영진 - 직원 전체 목록, 검색된 직원 정보 + 프로젝트 내역
 // router.get(
 //   "/searchEmployee",
 //   "/searchEmployee/:name",
@@ -37,7 +49,75 @@ router.get("/searchEmployee", function (req, res, next) {
 //     }
 //   }
 // );
-
+// 요구사항 5번) 경영진은 관리 페이지에서 직원 검색을 통해 해당 직원의 과거 프로젝트 이력을 볼 수 있다.
+// 특정 직원 검색(post): 검색한 직원에 대한 정보만 나옴. + 참여한 프로젝트 정보 표시
+router.post("/searchEmployee", async (req, res, next) => {
+  const { selected_search_key, search_key } = req.body;
+  console.log(selected_search_key, search_key);
+  if (selected_search_key == "id") {
+    try {
+      const employee = await Employee.findOne({
+        where: {
+          id: search_key,
+        },
+      });
+      const result = await Emp_Proj.findAll({
+        include: [
+          {
+            model: Employee,
+            attribute: ["id"],
+            where: { id: search_key },
+          },
+          {
+            model: Project,
+          },
+          {
+            model: Role,
+          },
+        ],
+      });
+      res.render("manager/searchEmployee", {
+        title: "searchEmployee",
+        employee,
+        result,
+      });
+    } catch (error) {
+      console.error(error);
+      return next(error);
+    }
+  } else {
+    try {
+      const employee = await Employee.findOne({
+        where: {
+          emp_name: search_key,
+        },
+      });
+      const result = await Emp_Proj.findAll({
+        include: [
+          {
+            model: Employee,
+            attribute: ["id"],
+            where: { id: employee.id },
+          },
+          {
+            model: Project,
+          },
+          {
+            model: Role,
+          },
+        ],
+      });
+      res.render("manager/searchEmployee", {
+        title: "searchEmployee",
+        employee,
+        result,
+      });
+    } catch (error) {
+      console.error(error);
+      return next(error);
+    }
+  }
+});
 // 경영진 - 프로젝트검색
 router.get("/manageAllProject", function (req, res, next) {
   res.render("manager/manageAllProject", {
